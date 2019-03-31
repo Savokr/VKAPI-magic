8e93624ee906ce7fd69696ae75cd4e5ac55e7b99
Savelii Novikov <45129444+Savokr@users.noreply.github.com>
2019-03-31 19:58:24 +0200

    Update Wallreq.py

--- Wallreq.py
+++ Wallreq.py
@@ -1,6 +1,6 @@
 import time, vk
 
-session = vk.Session(access_token='040be1e2040be1e2040be1e235046cee8d0040b040be1e25fe4ce9a170dd5813a8c6acc')
+session = vk.Session(access_token='your_token')
 api = vk.API(session, v='5.87',  lang='ru', timeout=10)
 owners_id = input("Enter group/user id: ")
 post_id = input("Enter post id: ")
